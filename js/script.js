3dbf12b9f5104c5a183afd34f92a48c09326b916
Jhonatas <costajhonatas9@gmail.com>
2019-11-24 19:46:01 +0000

    add do code cheio de gambiarras para fazer add e rem no valor total

--- js/script.js
+++ js/script.js
@@ -19,3 +19,142 @@ function diminui(a){
 	}
 	document.getElementById(a).value = valor;
 }
+
+function CD1a(){
+	var total = document.getElementById("valorfinal").value;
+	if (total == "") {
+		total = 61;
+	}
+	else {
+		total = parseInt(total)
+		total = total + 61;		
+	}
+	document.getElementById("valorfinal").value = total;
+}
+function CD1d(){
+	var total = document.getElementById("valorfinal").value;
+	if (total > 0) {
+		total = parseInt(total);
+		total = total - 61;
+	}
+	if (total > 0) {
+		document.getElementById("valorfinal").value = total;
+	}else{
+		document.getElementById("valorfinal").value = total;
+	}
+}
+function CD2a(){
+	var total = document.getElementById("valorfinal").value;
+	if (total == "") {
+		total = 152;
+	}
+	else {
+		total = parseInt(total)
+		total = total + 152;		
+	}
+	document.getElementById("valorfinal").value = total;
+}
+function CD2d(){
+	var total = document.getElementById("valorfinal").value;
+	if (total > 0) {
+		total = parseInt(total);
+		total = total - 152;
+	}
+	if (total > 0) {
+		document.getElementById("valorfinal").value = total;
+	}else{
+		document.getElementById("valorfinal").value = total;
+	}
+}
+function CD3a(){
+	var total = document.getElementById("valorfinal").value;
+	if (total == "") {
+		total = 301;
+	}
+	else {
+		total = parseInt(total)
+		total = total + 301;		
+	}
+	document.getElementById("valorfinal").value = total;
+}
+function CD3d(){
+	var total = document.getElementById("valorfinal").value;
+	if (total > 0) {
+		total = parseInt(total);
+		total = total - 301;
+	}
+	if (total > 0) {
+		document.getElementById("valorfinal").value = total;
+	}else{
+		document.getElementById("valorfinal").value = total;
+	}
+}
+function CD4a(){
+	var total = document.getElementById("valorfinal").value;
+	if (total == "") {
+		total = 256;
+	}
+	else {
+		total = parseInt(total)
+		total = total + 256;		
+	}
+	document.getElementById("valorfinal").value = total;
+}
+function CD4d(){
+	var total = document.getElementById("valorfinal").value;
+	if (total > 0) {
+		total = parseInt(total);
+		total = total - 256;
+	}
+	if (total > 0) {
+		document.getElementById("valorfinal").value = total;
+	}else{
+		document.getElementById("valorfinal").value = total;
+	}
+}
+function CD5a(){
+	var total = document.getElementById("valorfinal").value;
+	if (total == "") {
+		total = 152;
+	}
+	else {
+		total = parseInt(total)
+		total = total + 152;		
+	}
+	document.getElementById("valorfinal").value = total;
+}
+function CD5d(){
+	var total = document.getElementById("valorfinal").value;
+	if (total > 0) {
+		total = parseInt(total);
+		total = total - 152;
+	}
+	if (total > 0) {
+		document.getElementById("valorfinal").value = total;
+	}else{
+		document.getElementById("valorfinal").value = total;
+	}
+}
+function CD6a(){
+	var total = document.getElementById("valorfinal").value;
+	if (total == "") {
+		total = 152;
+	}
+	else {
+		total = parseInt(total)
+		total = total + 152;		
+	}
+	document.getElementById("valorfinal").value = total;
+}
+function CD6d(){
+	var total = document.getElementById("valorfinal").value;
+	if (total > 0) {
+		total = parseInt(total);
+		total = total - 152;
+	}
+	if (total > 0) {
+		document.getElementById("valorfinal").value = total;
+	}else{
+		document.getElementById("valorfinal").value = total;
+	}
+}
