491f344af7dbe7e7199f4ea9b13b6907aab272bf
Jhonatas <costajhonatas9@gmail.com>
2019-11-24 18:48:12 +0000

    dell arqs do projeto passado e add do script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,21 @@
+// a fução aumenta e diminui fazem a atualização do valor
 function aumenta(a){
 	var valor = document.getElementById(a).value;
 	if(valor == "") {
-		valor = 1;
-	}
+        valor = 1;
+    }
 	else{
 		valor = parseInt(valor);
 		valor++;
+	}
+	document.getElementById(a).value = valor;
+}
+
+function diminui(a){
+	var valor = document.getElementById(a).value;
+	if(valor > 0) {
+		valor = parseInt(valor);
+		valor--;
 	}
 	document.getElementById(a).value = valor;
 }
